a738c6604b5b470d374160304827c513f0fa154b
peter <bonep@hotmail.com>
2016-07-26 07:02:02 +0800

    2

--- src/string/Try03Test.java
+++ src/string/Try03Test.java
@@ -7,7 +7,7 @@ import static org.testng.Assert.*;
 /**
  * Created by peter on 26/7/2016.
  */
-public class Try03Test {
+public class Try03Test { 
     @Test
     public void testNumber() throws Exception {
         assertTrue(1==1);
