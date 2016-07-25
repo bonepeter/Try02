3c699768451cde1d90fc00dc2fe9345dd6b5763c
peter <bonep@hotmail.com>
2016-07-26 06:53:50 +0800

    Initial commit

---
+++ src/string/Try03.java
@@ -0,0 +1,12 @@
+package string;
+
+/**
+ * Created by peter on 26/7/2016.
+ */
+public class Try03
+{
+    public int number(int n)
+    {
+        return n;
+    }
+}
---
+++ src/string/Try03Test.java
@@ -0,0 +1,16 @@
+package string;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by peter on 26/7/2016.
+ */
+public class Try03Test {
+    @Test
+    public void testNumber() throws Exception {
+        assertTrue(1==1);
+    }
+
+}
